bdd7e2c81140822356e2151caa1c07022ef33533
mzakharova <zakharovam@parliament.uk>
2017-04-12 13:18:04 +0100

    Update weightedtree_test.js

--- weightedtree/weightedtree_test.js
+++ weightedtree/weightedtree_test.js
@@ -194,7 +194,7 @@ function createDataTip(x,y,h1,h2,h3) {
 
 function onMeasure() {
    // Allows you to manually override vertical spacing
-   viz.tree().nodeSize([45,10]);
+   viz.tree().nodeSize([45,100]);
 }
 
 function onMouseOver(e,d,i) {

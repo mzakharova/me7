97491e1e8bfa9218d406a79ad83ff808374cec8f
hdaley <howarddaley@gmail.com>
2017-06-15 14:15:46 +0100

    Create weightedtree_test.js

--- weightedtree/weightedtree_test.js
+++ weightedtree/weightedtree_test.js
@@ -46,7 +46,7 @@ var valueField = "Budget";
 var valueFields = ["Budget", "State", "Local"];
 
 
-var formatCurrency = function (d) { if (isNaN(d)) d = 0; return "£" + d3.format(",.1r")(d) + " million"; };
+var formatCurrency = function (d) { if (isNaN(d)) d = 0; return "£ " + d3.format(",.1f")(d) + " million"; };
 
 function loadData() {
 
